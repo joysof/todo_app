4cc3e4a3fa796f36ac15890ee8ecdab8baae2917
sajib Hossen <sajibhossen560@gmail.com>
2026-06-24 20:05:33 +0600

    update code stacher

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/screens/home_page.dart';
 
-
-void main(){
+void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World this test',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
